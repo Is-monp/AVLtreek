74ff862556d4eff2151c3f963b970de24d599671
Is-monp <isabellapalencia@uninorte.edu.co>
2024-03-17 11:12:52 -0500

    Cambio en la distribucion
    -main page mejor optimizada.
    -Comienzo de funcionalidad de botones.

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import flet as ft
-from nav_rail import Navigation_rail
 from tabs import Tabs
 
 def main(page: ft.Page):
     page.theme_mode = "dark"
-    n_rail = Navigation_rail()
     t = Tabs()
     main_display=ft.Container(
         content=ft.Text("Area para mostrar cosas", size=20, color="Black"),
@@ -14,23 +12,36 @@ def main(page: ft.Page):
         bgcolor=ft.colors.WHITE,
         width=900,
         height=600        
+    )    
+    header=ft.Container(
+        content=ft.Text("Header", size=20, color="black"),
+        alignment=ft.alignment.center,
+        bgcolor=ft.colors.GREEN_400,
+        width=1400,
+        height=50
     )
     galery = ft.Container(
         content=ft.Text("Ver foto del nodo", size=10, color="Black"),
         alignment=ft.alignment.center,
-        bgcolor=ft.colors.BLUE_100,
-        width=250,
+        bgcolor=ft.colors.GREEN_100,
+        width=320,
         height=250
     )
+    
     SECONDARY_GUI = ft.Container(
         margin=ft.margin.only(bottom=11),
         expand=True,
         content=ft.Column([t,galery])
     )
-    MAIN_GUI = ft.Container(
+    components = ft.Container(
         margin=ft.margin.only(bottom=40),
         expand=True,
-        content=ft.Row([n_rail,main_display, SECONDARY_GUI])
+        content=ft.Row([main_display, SECONDARY_GUI])
+    )
+    
+    MAIN_GUI = ft.Container(
+        content=ft.Column([header, components]),
+        expand=True
     )
     page.add(MAIN_GUI)
     page.update()
--- tabs.py
+++ tabs.py
@@ -1,28 +1,63 @@
 import flet as ft
+
+color_dropdown = ft.Dropdown(
+    width=200,
+    label="Filtro",
+    hint_text="Selecciona el filtro:",
+    options=[
+        ft.dropdown.Option("Por type "),
+        ft.dropdown.Option("Por size"),
+    ],
+)    
+
+header=ft.Container(
+    content=ft.Text("Header", size=20, color="black"),
+    alignment=ft.alignment.center,
+    bgcolor=ft.colors.GREEN_400,
+    width=20,
+    height=20
+)
+    
 def Tabs():
     t = ft.Tabs(
-        selected_index=0,
+        selected_index=3,
         animation_duration=300,
+        indicator_color=ft.colors.GREEN_400, label_color=ft.colors.GREEN_200,
         tabs=[
             ft.Tab(
                 content=ft.Container(
-                    content=ft.Text("This is Tab 1"), alignment=ft.alignment.center
+                    content=ft.Column([ft.Text("this is tab1")])
                 ),
-                icon=ft.icons.ADD,
+                icon=ft.icons.ADD_CIRCLE_OUTLINED,
+                text="Add node"
             ),
             ft.Tab(
-                content=ft.Text("This is Tab 2"),icon=ft.icons.MINIMIZE
+                content=ft.Container(
+                    content=ft.Text("This is Tab 2"), alignment=ft.alignment.center,
+                ),
+                icon=ft.icons.DELETE_FOREVER_OUTLINED,
+                text="Delete node"
             ),
             ft.Tab(
-                content=ft.Text("This is Tab 3"),
-                icon=ft.icons.SEARCH
+                content=ft.Container(
+                    content=ft.Text("This is Tab 3"), alignment=ft.alignment.center
+                ),
+                icon=ft.icons.SEARCH,
+                text="Search node"
             ),
             ft.Tab(
-                content=ft.Text("This is Tab 4"), icon=ft.icons.SWIPE_OUTLINED
+                content=ft.Container(
+                    content=ft.Column([color_dropdown]),alignment=ft.alignment.center
+                ), icon=ft.icons.FILTER_ALT_OUTLINED,
+                text="Filter nodes"
             ),
             
             ft.Tab(
-                content=ft.Text("This is Tab 5"),icon=ft.icons.MOVE_DOWN_ROUNDED
+                content=ft.Container(
+                    content=ft.Text("This is Tab 5"), alignment=ft.alignment.center
+                ),icon=ft.icons.MOVE_DOWN_ROUNDED,
+                text="Level order traversal"
+                
             ),
             ft.Tab(
                 content=ft.Text("This is Tab 6"), icon=ft.icons.CONTENT_PASTE_SEARCH
@@ -30,3 +65,7 @@ def Tabs():
         ],
         expand=1,)
     return t
+
+    def button_clicked(e):
+        output_text.value = f"Dropdown value is:  {color_dropdown.value}"
+        page.update()
