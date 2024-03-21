080d8902e92df21111cbc105fdcdcb421eef9a67
Is-monp <isabellapalencia@uninorte.edu.co>
2024-03-20 23:15:49 -0500

    display fixed

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import flet as ft
-from tabs import Tabs, get_Nodoabuscar
+from tabs import Tabs
 from visuals import Slider, images_slider
+
+def tree_display(image_path:str):
+    main_display=ft.Container(
+        content=ft.Image(
+            src=image_path,
+            width=800,
+            height=650,
+            fit=ft.ImageFit.COVER,
+            repeat=ft.ImageRepeat.NO_REPEAT,
+        ),
+        margin=5,
+        padding=10,
+        alignment=ft.alignment.center,
+        width=800,
+        height=650        
+    ) 
+    return main_display      
+    
 
 def main(page: ft.Page):
 #Deficion de los componentes principales de la GUI.
     page.theme_mode = ft.ThemeMode.LIGHT
     tabs = Tabs()
     this_slider=Slider(page)
+    this_main=tree_display("./data/cats/cat.1.jpg")
     images = ft.Row(expand=1, wrap=False, scroll="always")
     route_list=[f"./data/cats/cat.{i}.jpg" for i in range(1, 20)]
-    
-    #Display principal para mostrar el arbol.
-    main_display=ft.Container(
-        content=ft.Text("Area para mostrar cosas", size=20, color="Black"),
-        margin=5,
-        padding=10,
-        alignment=ft.alignment.center,
-        bgcolor=ft.colors.WHITE,
-        width=800,
-        height=650        
-    )  
+
+
     #Header con el nombre del proyecto y el slider para cambiar el tema.
     header=ft.Container(
         content=ft.Row([ft.Icon(name=ft.icons.ACCOUNT_TREE_ROUNDED, color=ft.colors.GREEN_500,size=50),ft.Text("ALVTrek", weight=ft.FontWeight.BOLD,size=20),this_slider]),
@@ -32,7 +42,6 @@ def main(page: ft.Page):
     gallery = ft.Container(
         content=images,
         alignment=ft.alignment.center,
-        #bgcolor=ft.colors.GREEN_100,
         margin=ft.margin.only(bottom=18),
         width=400,
         height=270
@@ -65,7 +74,7 @@ def main(page: ft.Page):
     components = ft.Container(
         margin=ft.margin.only(bottom=10),
         expand=True,
-        content=ft.Row([tables, main_display, SECONDARY_GUI])
+        content=ft.Row([tables, this_main, SECONDARY_GUI])
     )
     
     MAIN_GUI = ft.Container(
